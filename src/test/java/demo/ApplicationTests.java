3c8ae809747587a49603a5dd5bb283e7aa789af4
Nhu Nguyen <nhu313@gmail.com>
2014-09-18 17:44:59 -0700

    Fixed application integration test

--- src/test/java/demo/ApplicationTests.java
+++ src/test/java/demo/ApplicationTests.java
@@ -2,11 +2,13 @@ package demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
+@IntegrationTest({"debug=true", "server.port=9000"})
 public class ApplicationTests {
 
 	@Test
